80457b3cb41a8bde255efd967c85009a3c1ca942
Sikander Ali <112273879+SIKANDERKUMBHAR@users.noreply.github.com>
2024-09-18 23:57:55 +0500

    added

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
                         fontWeight: "bolder",
                     }}
                 >
-                    10WeeksofCloudOps - TODO LIST
+                    SignDevOps-Cloud-Ops - TODO LIST
                 </Row>
   
                 <hr />
